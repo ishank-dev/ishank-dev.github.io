15f596f66de8188a6b92370a64f44cb16bb1c3a0
Ishank Sharma <ishankdev@gmail.com>
2020-12-02 15:39:54 +0530

    animation cursor

--- js/easterEgg.js
+++ js/easterEgg.js
@@ -1,3 +1,52 @@
+var count = 0;
+function toggle() {
+  count+=1;
+  var score = 0
+if(count == 3){
+  alert('Congratulations! You have unlocked an easter egg')
+  init();
+  newContent = document.querySelector('.summary');
+  message = '<h1>Pokemon Easter Egg</h1><img id = "party" src = "https://emojis.slackmojis.com/emojis/images/1563480763/5999/meow_party.gif?1563480763"><p>Use the arrow keys on your keyboard to move move your pikachu around</p>'
+  newContent.innerHTML = message;
+
+  var x = document.getElementById('musicMario');
+  x.play();
+  
+  var y = document.getElementById('music');
+  y.pause();
+  
+  pikachu = document.getElementById("player");
+  coin = document.getElementById("coin")
+  
+  document.getElementById('score').style.display = 'block';
+  
+  if(pikachu.style.display == 'block'){
+	pikachu.style.display = 'none';
+  }
+  else{
+	pikachu.style.display = 'block'
+  }
+  if(coin.style.display == 'block'){
+	coin.style.display = 'none';
+  }
+  else{
+	coin.style.display = 'block'
+  }
+}
+else{
+  var el = document.getElementById("stylesheet1");
+	if (el.href.match("css/style_dark.css")) {
+	  el.href = "css/style.css";
+
+	}
+	else {
+	  el.href = "css/style_dark.css";
+
+	}
+  }
+
+}
+
 function isTouching(a, b) {
 	const aRect = a.getBoundingClientRect();
 	const bRect = b.getBoundingClientRect();
